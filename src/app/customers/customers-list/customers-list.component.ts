70af97479730e6e33ba94d0db7ffe41258c866c5
Sean Charoutanh <scharoutanh@gmail.com>
2021-09-03 16:54:47 -0600

    added data binding with input property to cusomers html

--- src/app/customers/customers-list/customers-list.component.ts
+++ src/app/customers/customers-list/customers-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, Input } from "@angular/core";
 
 import { ICustomer } from "src/app/shared/interfaces";
 
@@ -7,6 +7,18 @@ import { ICustomer } from "src/app/shared/interfaces";
     templateUrl: './customers-list.component.html'
 })
 export class CustomersListComponent implements OnInit {
+    private _customers: ICustomer[] = [];
+    @Input() get customers(): ICustomer[] {
+        return this._customers;
+    }
+    
+    set customers(value: ICustomer[]) {
+        if (value) {
+            this.filteredCustomers = this._customers = value;
+            this.calculateOrders();
+        }
+    }
+
     filteredCustomers: any[] = [];
     customersOrderTotal!: number;
     currencyCode: string = 'USD';
@@ -17,12 +29,16 @@ export class CustomersListComponent implements OnInit {
         
     }
 
-    /*
+  
     calculateOrders() {
         this.customersOrderTotal = 0;
-        this.filteredCustomers.forEach((cust: ICustomer) => {
+        this.filteredCustomers.forEach((cust) => {
             this.customersOrderTotal += cust.orderTotal;
         });
     }
-    */
+  
+
+    sort(prop: string) {
+        // A sorter service will handle the sorting 
+    }
 }
