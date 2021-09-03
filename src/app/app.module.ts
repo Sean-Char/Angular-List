017bd8167faf30fd53909a076277146566ec7c3b
Sean Charoutanh <scharoutanh@gmail.com>
2021-09-02 20:52:28 -0600

    Created Customer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomersModule } from './customers/customers.module';
 
 @NgModule({
   declarations: [
@@ -10,6 +11,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    CustomersModule,
     AppRoutingModule
   ],
   providers: [],
